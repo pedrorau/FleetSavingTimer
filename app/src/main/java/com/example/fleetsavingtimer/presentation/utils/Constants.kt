c0034e9dabd99b414e1843ee449fe9eb71c693d0
Pedro Rau <pedroraudelacruz@gmail.com>
2025-04-28 23:55:35 -0500

    chore: update dialog content text and palette colors

--- app/src/main/java/com/example/fleetsavingtimer/presentation/utils/Constants.kt
+++ app/src/main/java/com/example/fleetsavingtimer/presentation/utils/Constants.kt
@@ -14,10 +14,10 @@ const val EMPTY_ALARM_TITLE_MESSAGE = "Verifique que los campos esten completos"
 const val FLEET_SAVE_TIMER = "Fleet Save Timer"
 const val DIALOG_TITLE = "Información"
 const val DIALOG_BUTTON_TEXT = "Aceptar"
-const val DIALOG_CONTENT = "Nuestra aplicación te ayuda a generar alarmas en el reloj del" +
-        "dispositivo, facilitando el cálculo de la hora aproximada para el fleet saving. En la" +
-        "sección de configuración, puedes personalizar el mensaje de la alarma (por" +
-        "defecto: 'Fleet Saving') y ajustar el tiempo de anticipación para la alarma, que por" +
+const val DIALOG_CONTENT = "Nuestra aplicación te ayuda a generar alarmas en el reloj del " +
+        "dispositivo, facilitando el cálculo de la hora aproximada para el fleet saving. En la " +
+        "sección de configuración, puedes personalizar el mensaje de la alarma (por " +
+        "defecto: 'Fleet Saving') y ajustar el tiempo de anticipación para la alarma, que por " +
         "defecto es de 5 minutos antes del fleet saving."
 const val NOT_OK_HOURS = "Solo se puede colocar numeros entre 0 y 24"
 const val NOT_OK_MINUTES = "Solo se puede colocar numeros entre 0 y 59"
--- app/src/main/java/com/example/fleetsavingtimer/ui/theme/Color.kt
+++ app/src/main/java/com/example/fleetsavingtimer/ui/theme/Color.kt
@@ -10,6 +10,14 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val primary = Color(0xFFA9B5DF)
-val secondary = Color(0xFF7886C7)
-val accent = Color(0xFF2D336B)
+//val primary = Color(0xFFA9B5DF)
+//val secondary = Color(0xFF7886C7)
+//val accent = Color(0xFF2D336B)
+
+//val primary = Color(0xFFF3C623)
+//val secondary = Color(0xFFEB8317)
+//val accent = Color(0xFF12375C)
+
+val primary = Color(0xFFFFFFFF)
+val secondary = Color(0xFFE68D48)
+val accent = Color(0xFF323650)
